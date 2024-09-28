35654540a5b52b55c0e275b3cbbde938358602d4
jasonmp4 <123483177+mendozarosso@users.noreply.github.com>
2024-09-27 20:08:42 -0400

    New accidentes git

---
+++ Program.cs
@@ -0,0 +1,16 @@
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using MiProyectoBlazor;
+using Blazored.Toast; 
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+
+builder.Services.AddBlazoredToast();
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+await builder.Build().RunAsync();
